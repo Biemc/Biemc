e2c507074145cd0f9ea2491084c4d5c2ff65cfad
David <David@Dis.Speedport_W_724V_Typ_A_05011602_00_001>
2014-08-15 17:33:47 +0200

    Added project properly

--- Biemc-Java/src/SWCell.java
+++
@@ -1,8 +0,0 @@
-
-public class SWCell {
-	int score;
-	SWCell above = null;
-	SWCell leftabove = null;
-	SWCell left = null;
-
-}
--- Biemc-Java/src/SmithWaterman.java
+++
@@ -1,7 +0,0 @@
-
-public class SmithWaterman {
-
-	String sequence1;
-	String sequence2;
-	SWCell[][] matrix;
-}
